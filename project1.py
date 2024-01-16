2b4e772aa439dbbcfa1e733ba1a8cc2bd831ece7
Dauli <alexisangedauli@gmail.com>
2024-01-16 18:05:52 +0200

    original readme file deleted

--- project1.py
+++ project1.py
@@ -1,8 +1,5 @@
 # Project of PIG Game
 
-# By Dauli,  CS Student at University of the People
-# Credit to TIM, 2023, (https://www.youtube.com/watch?v=21FnnGKSRZo)
-
 # Wikipedia: Pig is a simple dice game first described in print by John Scarne in 1945. 
 # Players take turns to roll a single dice as many times as they wish, 
 # adding all roll results to a running total, but losing their gained 
