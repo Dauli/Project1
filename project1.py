835f2ae3edbe009c3e74d5b8935994dd13bdae0e
Dauli <alexisangedauli@gmail.com>
2024-01-23 17:50:25 +0200

    removing some comments

--- project1.py
+++ project1.py
@@ -1,19 +1,14 @@
 # Project of PIG Game
 
-# Wikipedia: Pig is a simple dice game first described in print by John Scarne in 1945. 
-# Players take turns to roll a single dice as many times as they wish, 
-# adding all roll results to a running total, but losing their gained 
-# score for the turn if they roll a 1.
-
 # In this game, everyone has a turn
 # User need to enter number of players
 # then each player has a turn where he has roll the dine,
-# when u rolled, you gat a random number from 1 to 6,
+# when u rolled, you got a random number from 1 to 6,
 # if you get anything beside one, which mean between 2 to 6,
 # you get that number you add it to your score, for example,
 # you rolled 5, you get 5, then you rolled again you get 3, you score
 # will be 5 + 3, and so on.
-# But when you get a 1, you score wipped to a zero.
+# But when you get a 1, your score wipped to a zero.
 
 
 # We start by generating a radom number between 1 - 6
@@ -30,14 +25,11 @@ def roll():
 
 # continue to ask user for a valid number of players
 while True:
-    # Get number of players from user
     players = input('Enter the number of players (1-4): ')
 
     # if input is a number, convert it to int
     if players.isdigit():
         players = int(players)
-
-        # check if the number is between 1 and 4
         if players >= 2 and players <= 4:
             break
         else:
@@ -51,7 +43,6 @@ max_score = 50
 player_scores = [0 for _ in range(players)]
 
 while max(player_scores) < max_score:
-    
     for player_idx in range(players):
         print('\nPlayer number', player_idx + 1, 'turn has just started!\n')
         current_score = 0
