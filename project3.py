5c40503c88f8be416e23a849f96c54f8ec2f444b
Dauli <alexisangedauli@gmail.com>
2024-01-31 10:28:50 +0200

    checking the accurancy and finishing the game

--- project3.py
+++ project3.py
@@ -3,6 +3,7 @@
 # them answer until they get the correct answers
 
 import random
+import time
 
 # Constant variables
 OPERATORS = ['*', '+', '-']
@@ -21,6 +22,13 @@ def generate_problem():
 
     return expr, answer
 
+# Check accurancy of the answer
+wrong = 0
+input('Press "Enter" key to start')
+print('-----------------------------------')
+
+start_time = time.time()
+
 # Loop in range of problems and ask user
 for i in range(TOTAL_PROBLEMS):
     expr, answer = generate_problem()
@@ -29,3 +37,10 @@ for i in range(TOTAL_PROBLEMS):
         guess = input('Problem #' + str(i + 1) + ': ' + expr + ' = ')
         if guess == str(answer):
             break
+        wrong += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print('-----------------------------------')
+print('Nice work! You finished in', total_time, 'seconds!')
